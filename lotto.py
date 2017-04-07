4a37c7748374f4d7ddc68261eec8a1ebd0cc166e
Básthy Ádám <e15.basthy.adam@palfy-vizugyi.hu>
2017-04-07 11:43:55 +0200

    elsovaltozat

--- lotto.py
+++ lotto.py
@@ -4,6 +4,7 @@ while len(szamok)<5:
 	kihuzott=random.randrange(1,91)
 	if kihuzott not in szamok: szamok.append(kihuzott)
 szamok.sort()
+print("A kihúzott számok a következők: ",end="")
 for i in szamok:
 	print(i," ",end="")
 print("\n")
